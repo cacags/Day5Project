60c8c941ba9fa109cc538f12d1af22b05118d14a
Eyüp Caglar Keskin <65714110+cacags@users.noreply.github.com>
2020-10-13 19:26:26 +0200

    FROM GITHUB

--- src/test/java/Hello.java
+++ src/test/java/Hello.java
@@ -1,6 +1,6 @@
 public class Hello {
 
     public static void main(String[] args) {
-        System.out.println("sill really awesome hello");
+        System.out.println("sill really a FROM GITHUB MADE SOME CHANGES wesome hello");
     }
 }
