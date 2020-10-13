a22aaf6381330b4e0b4048839f116d2e01040d3a
Eyüp Caglar Keskin <65714110+cacags@users.noreply.github.com>
2020-10-13 19:39:36 +0200

    Update Hello.java

--- src/test/java/Hello.java
+++ src/test/java/Hello.java
@@ -1,6 +1,6 @@
 public class Hello {
 
     public static void main(String[] args) {
-        System.out.println("sill really awesome hello"); //added comment
+        System.out.println("sill really MY COWORKER ADDED awesome hello"); //WE ARE SIMULATING THE SITUATION
     }
 }
