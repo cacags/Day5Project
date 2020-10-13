a13e6e944229688fdb23ead5b4edbb80243b3c4c
Eyüp Caglar Keskin <65714110+cacags@users.noreply.github.com>
2020-10-13 19:47:17 +0200

    co-worker added some more changes here

--- src/test/java/Hello.java
+++ src/test/java/Hello.java
@@ -2,5 +2,6 @@ public class Hello {
 
     public static void main(String[] args) {
         System.out.println("sill really MY COWORKER ADDED awesome hello"); //WE ARE SIMULATING THE SITUATION
+        //co-worker pushed some new changes here that I do not yet know about
     }
 }
