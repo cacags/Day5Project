627051bab14a9f1af39fea8e077739cfb184ac68
Caglar Keskin <caglarkeskin@gmail.com>
2020-10-13 19:32:11 +0200

    added comment on same line

--- src/test/java/Hello.java
+++ src/test/java/Hello.java
@@ -1,6 +1,6 @@
 public class Hello {
 
     public static void main(String[] args) {
-        System.out.println("sill really awesome hello");
+        System.out.println("sill really awesome hello"); //added comment
     }
 }
